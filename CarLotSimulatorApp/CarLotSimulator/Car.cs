8b28af1558e0b2050f52315e766ddc109ee8812a
Thai Vo <tahrbo@gmail.com>
2025-07-04 12:35:41 -0500

    Starting out

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car() { }
+
+        public string Make;
+        public string Model;
+        public int Year;
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+
+        public bool IsDriveable { get; set; }
+
+    }
+}
